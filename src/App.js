99db1033598414f81d728e05ba310ae32afb88cb
suparerk <ch.suparerk@gmail.com>
2022-01-26 22:08:52 +0700

    Create board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class Cell extends Component {
+  render() {
+    return <div className='cell'>{this.props.number}</div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='board'>
+        <Cell number={2}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+        <Cell number={3}/>
+      </div>
     </div>
   );
 }
