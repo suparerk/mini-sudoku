5f0e2765e28bf5041339492b59d0eb0ac0bcb13f
suparerk <ch.suparerk@gmail.com>
2022-01-26 22:39:32 +0700

    Board and Cell component

--- src/App.js
+++ src/App.js
@@ -3,32 +3,56 @@ import './App.css';
 import { Component } from 'react';
 
 class Cell extends Component {
+  state = {
+    number: 1
+  }
   render() {
-    return <div className='cell'>{this.props.number}</div>
+    return <div
+              onClick={(e) => {
+                this.setState({
+                  number: (this.props.number + 1) % 5
+                })
+              }}
+              className={`cell ${this.props.isInitial ? 'initial' : ''}`}
+            >
+              {this.props.number !== 0 && this.props.number}
+            </div>
+  }
+}
+
+class Board extends Component {
+  state = {
+    board: [[1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4]],
+    initial: [
+      [true, false, true, false],
+      [true, false, true, false],
+      [true, false, true, false],
+      [true, false, true, false],
+    ]
+  }
+  render() {
+    return (
+      <div className='board'>
+        {
+          this.state.board.map((row, i) => 
+            row.map((number, j) => (
+              <Cell
+                key={`cell-${i}-${j}`}
+                number={number}
+                isInitial={this.state.initial[i][j]}
+              />
+            ))
+          )
+        }
+      </div>
+    )
   }
 }
 
 function App() {
   return (
     <div className="App">
-      <div className='board'>
-        <Cell number={2}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-        <Cell number={3}/>
-      </div>
+      <Board />
     </div>
   );
 }
